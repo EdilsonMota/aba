66b6f6c1771e9c9b8e04cdb91d9774b5ec99863a
Edilson Mota <edilson.mota3@hotmail.com>
2023-08-25 17:32:09 +0100

    Actualizar projecto

--- script/main.js
+++ script/main.js
@@ -1,2 +1,6 @@
-const myHeading = document.querySelector('p');
-myHeading.textContent = 'Hello world!';
+
+// Adicionar espaço entre hamburguer menu e o topo automaticamente
+const height = document.getElementById("top-nav").getComputedStyle()
+const menu = div.querySelector("#menu");
+const menuPositino = window.getComputedStyle(div)["height"];
+
