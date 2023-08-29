8521dd7a20728e9c57e670157ff70afebb4bafdd
Edilson Mota <edilson.mota3@hotmail.com>
2023-08-29 08:49:57 +0100

    Mais alguns ajustes ao layout. Parece-me bem.

--- script/main.js
+++ script/main.js
@@ -4,3 +4,13 @@ const height = document.getElementById("top-nav").getComputedStyle()
 const menu = div.querySelector("#menu");
 const menuPositino = window.getComputedStyle(div)["height"];
 
+function enviarEmail(){
+    const textoEmail = getElementById();
+    var mailSplitted = ['mailto', 'to:mye', 'mail@', 'addre', 'ss.fr'];
+    var destinatario = "mail@example.org";
+    var assunto = "?subject=Atendimento";
+    var cc = "";
+    var bcc = "edilson.mota3@hotmail.com";
+    var corpo = ;
+    window.location.href = "mailto:mail@example.org?";
+}
